1c93f95e68f69086f13afb322b957dc031892aab
Adam Seligson <adamseligson@hotmail.com>
2018-11-12 19:22:49 -0500

    Database push

---
+++ 42-Personal-Project/src/database/VoiceRange.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS voice_range;
+DROP SEQUENCE IF EXISTS voice_range_id;
+
+CREATE SEQUENCE seq_voice_range_id;
+
+CREATE TABLE voice_ranges
+(
+	voice_range_id INTEGER PRIMARY KEY DEFAULT NEXTVAL('voice_range_id'),
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+    voice_range VARCHAR(200) NOT NULL
+);
+
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('John', 'Johnson', 'Bass');
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('Ham', 'Sandwich', 'Baritone');
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('Bilbo', 'Baggins', 'Tenor');
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('Andrew', 'Johnson', 'Soprano');
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('Jimmy', 'Crackcorn', 'Castrati');
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('Ivanna', 'Humpalot', 'Soprano');
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('Richard', 'Hertz', 'Alto');
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('Gottfried', 'Leibniz', 'Alto');
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('Hugh', 'Jaynus', 'Baritone');
+INSERT INTO voice_ranges(first_name, last_name, voice_range) VALUES ('Bill', 'Dabearworkshop', 'Tenor');
+
+
+
+
--- 42-Personal-Project/src/main/java/com/techelevator/HelloController.java
+++ 42-Personal-Project/src/main/java/com/techelevator/HelloController.java
@@ -4,14 +4,22 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller 
 public class HelloController {
 
-	@RequestMapping("/greeting")
+	@RequestMapping(path= {"/", "greeting"})
 	public String displayGreeting() {
 		
 		return "greeting";
 	}
+	@RequestMapping(path="something", method=RequestMethod.GET)
+	
+	public String something() {
+		
+		return "something";
+	}
+	
 }
